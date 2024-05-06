81e4f47bb0f88d422760f0b55df764d104ea7059
Bartosz Wojnarowski <bartosz.wojnarowski@o2.pl>
2024-05-06 21:47:21 +0200

    during modyfications

--- DesktopClient/App.xaml.cs
+++ DesktopClient/App.xaml.cs
@@ -9,6 +9,14 @@ namespace DesktopClient
     /// </summary>
     public partial class App : Application
     {
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            Console.WriteLine( "here?");
+        }
+        public App()
+        {
+            Console.WriteLine("here we ara");
+        }
     }
 
 }
