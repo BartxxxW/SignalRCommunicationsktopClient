c34164f260362e35616ba760044bbf9188b7acb9
Bartosz Wojnarowski <bartosz.wojnarowski@o2.pl>
2024-05-06 13:49:38 +0200

    first commit

---
+++ DesktopClient/MainWindow.xaml.cs
@@ -0,0 +1,80 @@
+﻿using System.Text;
+using System;
+using System.Threading.Tasks;
+using System.Windows;
+using Microsoft.AspNetCore.SignalR.Client;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace DesktopClient
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        HubConnection connection;
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            connection = new HubConnectionBuilder()
+                .WithUrl("https://localhost:7266/messageHub")
+                .Build();
+
+            connection.Closed += async (error) =>
+            {
+                await Task.Delay(new Random().Next(0, 5) * 1000);
+                await connection.StartAsync();
+            };
+
+            Connect();
+
+        }
+
+        public async void Connect()
+        {
+            connection.On<string>("ReceiveMessage", (message) =>
+            {
+                this.Dispatcher.Invoke(() =>
+                {
+                    ReceivedMessage.Content= message;
+                });
+            });
+
+            try
+            {
+                await connection.StartAsync();
+                StatusLabel.Content = "Connected";
+            }
+            catch (Exception ex)
+            {
+                StatusLabel.Content = "Not Connected";
+            }
+        }
+
+        private async  void Button_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                await connection.InvokeAsync("Send",
+                     SendMessage.Text);
+            }
+            catch (Exception ex)
+            {
+                ReceivedMessage.Content = $"Errod during sending message : {ex}";
+            }
+        }
+
+        private void TextBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,1 @@
+# SignalRCommunicationsktopClient
